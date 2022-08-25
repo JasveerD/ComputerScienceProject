ea8f8b367fce314d35aaa343011c77c96fd23974
Jasveer_D <93256850+Lil-Buddha@users.noreply.github.com>
2022-08-26 00:17:56 +0530

    Update pillow2.py

--- Photoshop Main/pillow2.py
+++ Photoshop Main/pillow2.py
@@ -11,4 +11,15 @@ image_rotate = image.rotate(60, expand=True, fillcolor=ImageColor.getcolor('red'
 image_crop = image.crop((0, 0, 500, 400))
 
 # flipping the image
+image_flip_horizontal = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+image_flip_vertical = image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
 
+# resize
+image_resized = image.resize((1000, 1000))  # arg = tuple
+
+# better resizing
+scale_factor = 2
+new_image_size = (image.size[0] * scale_factor, image.size[1] * scale_factor)
+better_resized_image = image.resize(new_image_size)
+
+better_resized_image.show()
